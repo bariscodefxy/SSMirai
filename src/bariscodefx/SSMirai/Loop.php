9ee85f354d1885102f0b17d5bf490e6ae2f9c39c
bariscodefxy <bariscodefx@protonmail.com>
2022-06-03 20:57:04 +0300

    Loop: add getTCPSniffer(), getUDPSniffer() functions

--- src/bariscodefx/SSMirai/Loop.php
+++ src/bariscodefx/SSMirai/Loop.php
@@ -20,21 +20,43 @@ class Loop {
 
 	public function startSniffing() {
 		while(true){
-			$tcp_packet = NULL;
-			$udp_packet = NULL;
+			$tcp_buffer = "TCP BUFFER";
+			$udp_buffer = "UDP BUFFER";
 			@socket_recv( $this->tcp->socket, $tcp_packet, 65536, 0 );
 			@socket_recv( $this->udp->socket, $udp_packet, 65536, 0 );
 
-			$this->process($tcp_packet, $this->tcp->socket);
-			$this->process($udp_packet, $this->udp->socket);
+			$this->process($tcp_packet, $this->tcp);
+			$this->process($udp_packet, $this->udp);
 		}
 	}
 
-	public function process($packet, $sock){
-		@socket_getpeername($sock, $ip, $port);
-		if(!in_array($ip, $this->IPs))
+	public function getTCPSniffer(): TCPSniffer {
+		return $this->tcp;
+	}
+
+	public function getUDPSniffer(): UDPSniffer {
+		return $this->udp;
+	}
+
+	public function process($packet, $sniffer){
+		if(!$packet || !$sniffer) return;
+		if($sniffer instanceof TCPSniffer)
 		{
-			print_r($this->IPs);
+			$ip_header_fmt = 'Cip_ver_len/'
+			.'Ctos/'
+			.'ntot_len/'
+			.'nidentification/'
+			.'nfrag_off/'
+			.'Cttl/'
+			.'Cprotocol/nheader_checksum/Nsource_add/Ndest_add/';
+			$ip_header = unpack($ip_header_fmt , $packet);
+			if($ip_header['protocol'] == '6')
+	  		{
+	  			$sniffer->print_tcp_packet($packet);
+	  		}
+	  		echo "TCP packet \n";
+		}else if($sniffer instanceof UDPSniffer) {
+			echo "UDP packet \n";
 		}
 	}
 
